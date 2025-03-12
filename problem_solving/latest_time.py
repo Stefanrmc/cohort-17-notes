2668f4cd7d577e8046540551f54278a0aa15744e
Stefanrmc <stefanrcole@gmail.com>
2025-03-12 14:51:59 +0000

    Tests for problem solving workshop

--- problem_solving/latest_time.py
+++ problem_solving/latest_time.py
@@ -1,9 +1,13 @@
 def get_first_digit(digits: list[int]) -> int:
+    """Returns a digit between zero and two for the first digit of time"""
     ...
+
 
 def get_second_digit(digits: list[int], first_digit: int) -> int:
+    """Returns a digit between zero and nine if first_digit is not two, otherwise between zero and three"""
     ...
+
 
 def get_third_digit(digits: list[int]) -> int:
+    """Returns a digit between zero and five from a list"""
     ...
-
--- problem_solving/test_latest_time.py
+++ problem_solving/test_latest_time.py
@@ -4,14 +4,26 @@ from latest_time import (get_first_digit,
                          get_second_digit,
                          get_third_digit)
 
+
 def test_get_first_digit_returns_zero():
     assert get_first_digit([0, 9, 3, 8]) == 0
+
 
 def test_get_first_digit_returns_one():
     assert get_first_digit([1, 9, 8, 3]) == 1
 
+
 def test_get_first_digit_returns_two():
     assert get_first_digit([9, 1, 2, 5]) == 2
 
-@pytest.mark.parametrize(["first", "second", "third", "fourth"], 
+
+def test_get_second_digit_with_first_digit_two():
+    assert get_second_digit([2, 5, 9], 2) == 2
+
+
+def test_get_second_digit_with_first_digit_as_two_and_multiple_valid_digits():
+    assert get_second_digit([1, 0, 9], 2) == 1
+
+
+@pytest.mark.parametrize(["first", "second", "third", "fourth"],
                          ())
